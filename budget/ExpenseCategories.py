cf1f354e784210491b4b39f1e3857410b81d7f9a
peksha <peksha@seznam.cz>
2021-10-07 09:36:45 +0200

    Chapter 5, Categorizing expenses into separate sets, fix2

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -7,7 +7,7 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
-    if not divided_for_loop == divided_set_comp:
+    if not(divided_for_loop == divided_set_comp):
         print("Sets are NOT equal y == test")
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not(a.issubset(b) and b.issubset(a)):
