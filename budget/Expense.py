0a0d9ce974519e1b67190954c408aa8a88ae61a9
peksha <peksha@seznam.cz>
2021-10-07 09:34:43 +0200

    Chapter 5, Categorizing expenses into separate sets, fix

--- budget/Expense.py
+++ budget/Expense.py
@@ -47,7 +47,7 @@ class Expenses():
         necessary_expenses = {x for x in self.list
                               if x.category == "Phone" or
                               x.category == "Auto and Gas" or
-                              x.category == "Clases" or
+                              x.category == "Classes" or
                               x.category == "Utilities" or
                               x.category == "Mortgage"}
         food_expenses = {x for x in self.list
